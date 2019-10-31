97cf42ef601e63aa1374addd129ef89306f1f584
isaaczhou <isaaczhou85@gmail.com>
2019-10-31 00:29:48 -0400

    add helloworld for c++

--- clang/01-helloworld/helloworld.c
+++ clang/01-helloworld/helloworld.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
+    int a = 100;
+    float b = 5.89;
+    char c='Y';
     printf("Hello World!\n");
+    printf("a=%d\n", a);
+    printf("b=%f\n", b);
+    printf("c=%c\n", c);
     return 0;
 }
---
+++ cpp/01-helloworld/helloworld.cpp
@@ -0,0 +1,15 @@
+/**
+ * this is my another first C++ program
+ *
+ * @author Isaac Zhou
+ * @date 2019-10-31
+ * @copyleft GPL 2.0
+ */
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout<<"Hello World!"<<std::endl;
+    return 0;
+}
